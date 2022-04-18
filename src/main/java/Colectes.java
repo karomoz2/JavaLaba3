ae2b8aad9fc131961813f3aebc3c30566090e5c3
karomoz2 <karomoz2@yandex.ru>
2022-04-18 19:59:52 +0300

    0.021

---
+++ src/main/java/Colectes.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Colectes {
+
+    int c;
+    int b;
+
+    public  void TestAdd()
+    {
+        System.out.println("Привет");
+    }
+}
