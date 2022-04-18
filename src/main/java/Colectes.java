938c0ad592d239b4337d8eb5f57a494deab7751e
karomoz2 <karomoz2@yandex.ru>
2022-04-18 20:09:53 +0300

    ver 0.03

--- src/main/java/Colectes.java
+++ src/main/java/Colectes.java
@@ -5,11 +5,77 @@ import java.util.List;
 
 public class Colectes {
 
-    int c;
-    int b;
-
-    public  void TestAdd()
+    static void TestAdd(int n, List<Integer> list)
     {
-        System.out.println("Привет");
+        System.out.printf("%-10s%-,20d","Add",n);
+
+        long time = System.nanoTime();
+
+        for(int i=0;i<n;i++)
+            list.add(i);
+
+        time = System.nanoTime() - time;
+        System.out.printf("%-12.3f\n", time/1_000_000.0);
     }
+    private static void TestGet(int n, List<Integer> list)
+    {
+        System.out.printf("%-10s%-,20d","Get",n);
+
+        long time = System.nanoTime();
+
+        for(int i=0;i<n;i++)
+            list.get(i);
+
+        time = System.nanoTime() - time;
+        System.out.printf("%-12.3f\n", time/1_000_000.0);
+    }
+    private static void TestDelete(int n, List<Integer> list)
+    {
+        System.out.printf("%-10s%-,20d","Delete",n);
+
+        long time = System.nanoTime();
+
+        for(int i=0;i<n;i++)
+            list.remove(i);
+
+        time = System.nanoTime() - time;
+        System.out.printf("%-12.3f\n", time/1_000_000.0);
+    }
+
+    static void printListName(List<Integer> list)
+    {
+        if (list.getClass().toString().equals("class java.util.LinkedList"))
+            System.out.printf("%-15s", "LinkedList");
+        else
+            System.out.printf("%-15s", "ArrayList");
+    }
+    static void Testing(List<Integer> list)
+    {
+        for(int i=1000;i<=100000;i=i*10) {
+            printListName(list);
+            TestAdd(i, list);
+        }
+        System.out.println();
+        for(int i=1000;i<=20000;i=i*4) {
+            printListName(list);
+            TestGet(i, list);
+        }
+        System.out.println();
+        for(int i=1000;i<=20000;i=i*4) {
+            printListName(list);
+            TestDelete(i, list);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        ArrayList<Integer> array = new ArrayList<>();
+        System.out.printf("%-15s%-10s%-20s%-12s%n","List type","Method","Col-vo oper","Time(ms)");
+        Testing(list);
+        System.out.println();
+        Testing(array);
+    }
+
 }
