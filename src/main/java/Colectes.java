ad15f762879d591c53bad45a32890acdee2a9bdc
karomoz2 <karomoz2@yandex.ru>
2022-04-18 21:25:13 +0300

    Release 1.0
    finaly

--- src/main/java/Colectes.java
+++ src/main/java/Colectes.java
@@ -2,38 +2,40 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.lang.Object;
+
 
 public class Colectes {
-
+    //Замер добавления
     static void TestAdd(int n, List<Integer> list)
     {
-        System.out.printf("%-10s%-,20d","Add",n);
-
-
+        System.out.print("      Add");
+        System.out.print("         "+n);
         long time = System.nanoTime();
 
         for(int i=0;i<n;i++)
             list.add(i);
 
         time = System.nanoTime() - time;
-        System.out.printf("%-12.3f\n", time/1_000_000.0);
+        System.out.printf("           "+ time/1_000_000.0);
     }
+    //Замер получения
     private static void TestGet(int n, List<Integer> list)
     {
-        System.out.printf("%-10s%-,20d","Get",n);
-
+        System.out.print("      Get");
+        System.out.print("         "+n);
         long time = System.nanoTime();
 
         for(int i=0;i<n;i++)
             list.get(i);
 
         time = System.nanoTime() - time;
-        System.out.printf("%-12.3f\n", time/1_000_000.0);
+        System.out.printf("           "+ time/1_000_000.0);
     }
+    //Замер удаления
     private static void TestDelete(int n, List<Integer> list)
     {
-        System.out.printf("%-10s%-,20d","Delete",n);
+        System.out.print("      Delete");
+        System.out.print("      "+n);
 
         long time = System.nanoTime();
 
@@ -41,68 +43,106 @@ public class Colectes {
             list.remove(i);
 
         time = System.nanoTime() - time;
-        System.out.printf("%-12.3f\n", time/1_000_000.0);
+        System.out.printf("           "+ time/1_000_000.0);
     }
-
+    //Замер сортировки
     private static void TestSort(int n, List<Integer> list)
     {
-        System.out.printf("%-10s%-,20d","Sort",n);
-
+        //System.out.printf("%-10s%-,20d","Sort",n);
+        System.out.print("      Sort");
+        System.out.print("        "+n);
         long time = System.nanoTime();
 
         Collections.sort(list);
 
         time = System.nanoTime() - time;
-        System.out.printf("%-12.3f\n", time/1_000_000.0);
+        System.out.print("           "+ time/1_000_000.0);
     }
+    //Замер размера
+    private static void TestSize(int n, List<Integer> list)
+    {
+        //System.out.printf("%-10s%-,20d","Sort",n);
+        System.out.print("      Size");
+        System.out.print("        "+n);
+        long time = System.nanoTime();
+
+        list.size();
+
+        time = System.nanoTime() - time;
+        System.out.print("           "+ time/1_000_000.0);
+    }
+
 
     static void printListName(List<Integer> list)
     {
         if (list.getClass().toString().equals("class java.util.LinkedList"))
-            System.out.printf("%-15s", "LinkedList");
+            System.out.print("LinkedList");
         else
-            System.out.printf("%-15s", "ArrayList");
+            System.out.print("ArrayList ");
     }
     static void Testing(List<Integer> Linkedlist, List<Integer> Arraylist)
     {
-        for(int i=1000;i<=100000;i=i*10) {
+        System.out.println();
+        for(int i=1000;i<=8000;i=i*2) {
             printListName(Linkedlist);
             TestAdd(i, Linkedlist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=100000;i=i*10) {
+        for(int i=1000;i<=8000;i=i*2) {
             printListName(Arraylist);
             TestAdd(i, Arraylist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=20000;i=i*4) {
+        for(int i=1000;i<=8000;i=i*2) {
             printListName(Linkedlist);
             TestGet(i, Linkedlist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=20000;i=i*4) {
+        for(int i=1000;i<=8000;i=i*2) {
             printListName(Arraylist);
             TestGet(i, Arraylist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=20000;i=i*4) {
+        for(int i=1000;i<8000;i=i*2) {
             printListName(Linkedlist);
             TestDelete(i, Linkedlist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=20000;i=i*4) {
+        for(int i=1000;i<8000;i=i*2) {
             printListName(Arraylist);
             TestDelete(i, Arraylist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=20000;i=i*4) {
+        for(int i=1000;i<=8000;i=i*2) {
             printListName(Linkedlist);
             TestSort(i, Linkedlist);
+            System.out.println();
         }
         System.out.println();
-        for(int i=1000;i<=20000;i=i*4) {
+        for(int i=1000;i<=8000;i=i*2) {
             printListName(Arraylist);
             TestSort(i, Arraylist);
+            System.out.println();
+        }
+
+
+        System.out.println();
+        for(int i=1000;i<=8000;i=i*2) {
+            printListName(Linkedlist);
+            TestSize(i, Linkedlist);
+            System.out.println();
+        }
+        System.out.println();
+        for(int i=1000;i<=8000;i=i*2) {
+            printListName(Arraylist);
+            TestSize(i, Arraylist);
+            System.out.println();
         }
     }
 
@@ -111,10 +151,9 @@ public class Colectes {
     public static void main(String[] args) {
         LinkedList<Integer> listLinked = new LinkedList<>();
         ArrayList<Integer> arrayList = new ArrayList<>();
-        System.out.printf("%-15s%-10s%-20s%-12s%n","List type","Method","Col-vo oper","Time(ms)");
+        System.out.print("List type     Method      Col-vo oper     Time(ms)");
         Testing(listLinked, arrayList);
-        //System.out.println();
-        //Testing(array);
+
     }
 
 }
