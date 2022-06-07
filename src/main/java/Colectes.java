337abe1e05e36f5786893c93e056f91394f92005
karomoz2 <karomoz2@yandex.ru>
2022-06-07 16:35:16 +0300

    0.993
    Добавил три замера ADD GET DELL

--- src/main/java/Colectes.java
+++ src/main/java/Colectes.java
@@ -29,8 +29,8 @@ public class Colectes {
             arLi.add(i);
         }
         long EndTime= System.nanoTime()-StartTime;
-        EndTime=EndTime/1000000;
-        System.out.print(EndTime);
+       // EndTime=EndTime/1_000_000;
+        System.out.print(EndTime/1_000_000.0);
     }
     public  void AddLinked(int n)
     {
@@ -40,8 +40,57 @@ public class Colectes {
             linLi.add(i);
         }
         long EndTime= System.nanoTime()-StartTime;
-        EndTime=EndTime/100000;
-        System.out.print(EndTime);
+        //EndTime=EndTime/100000;
+        System.out.print(EndTime/1_000_000.0);
     }
+
+    public  void DelArr(int n)
+    {
+        long StartTime = System.nanoTime();
+        for(int i=0; i<n;i++)
+        {
+            arLi.remove(i);
+        }
+        long EndTime= System.nanoTime()-StartTime;
+        // EndTime=EndTime/1_000_000;
+        System.out.print(EndTime/1_000_000.0);
+    }
+
+    public  void DelLi(int n)
+    {
+        long StartTime = System.nanoTime();
+        for(int i=0; i<n;i++)
+        {
+            linLi.remove(i);
+        }
+        long EndTime= System.nanoTime()-StartTime;
+        // EndTime=EndTime/1_000_000;
+        System.out.print(EndTime/1_000_000.0);
+    }
+
+    public  void GetArr(int n)
+    {
+        long StartTime = System.nanoTime();
+        for(int i=0; i<n;i++)
+        {
+            arLi.get(i);
+        }
+        long EndTime= System.nanoTime()-StartTime;
+        // EndTime=EndTime/1_000_000;
+        System.out.print(EndTime/1_000_000.0);
+    }
+
+    public  void GetLi(int n)
+    {
+        long StartTime = System.nanoTime();
+        for(int i=0; i<n;i++)
+        {
+            linLi.get(i);
+        }
+        long EndTime= System.nanoTime()-StartTime;
+        // EndTime=EndTime/1_000_000;
+        System.out.print(EndTime/1_000_000.0);
+    }
+
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,8 +10,25 @@ public class main {
         int n=10;
         Colectes a=new Colectes();
 
-        a.AddArr(100000);
+        a.AddArr(10000);
+        System.out.println("");
+        System.out.println("Link!");
+        a.AddLinked(10000);
+        System.out.println("");
 
+        System.out.println("Get!");
+        a.GetArr(10000);
+        System.out.println("");
+        System.out.println("Link!");
+        a.GetLi(10000);
+        System.out.println("");
+
+        System.out.println("Delenn");
+        a.DelArr(1000);
+        System.out.println("");
+        System.out.println("Link!");
+        a.DelLi(1000);
+        System.out.println("");
 
 
 
