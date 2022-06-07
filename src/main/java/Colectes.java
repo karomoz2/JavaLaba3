97356b0dfa23676157216025b313439cb3c8a725
karomoz2 <karomoz2@yandex.ru>
2022-06-07 16:15:00 +0300

    Начало положено
    Начало положено..

--- src/main/java/Colectes.java
+++ src/main/java/Colectes.java
@@ -5,7 +5,43 @@ import java.util.List;
 
 
 public class Colectes {
-    
+    ArrayList<Integer> arLi=new ArrayList<>();
+    LinkedList<Integer> linLi=new LinkedList<>();
+   // int n;
 
+    public Colectes()
+    {
+       // arLi=null;
+        //linLi=null;
+      //  n=kolvo;
+    }
+   /* public Colectes(ArrayList<int> arrayList, LinkedList<int> linked, int kolvo)
+    {
+        arLi=arrayList;
+        linLi=linked;
+        n=kolvo;
+    }*/
+    public  void AddArr(int n)
+    {
+        long StartTime = System.nanoTime();
+        for(int i=0; i<n;i++)
+        {
+            arLi.add(i);
+        }
+        long EndTime= System.nanoTime()-StartTime;
+        EndTime=EndTime/1000000;
+        System.out.print(EndTime);
+    }
+    public  void AddLinked(int n)
+    {
+        long StartTime = System.nanoTime();
+        for(int i=0; i<n;i++)
+        {
+            linLi.add(i);
+        }
+        long EndTime= System.nanoTime()-StartTime;
+        EndTime=EndTime/100000;
+        System.out.print(EndTime);
+    }
 
 }
---
+++ src/main/java/main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class main {
+    public  static void main(String args[]) {
+        System.out.println("Vsem privet!");
+        int n=10;
+        Colectes a=new Colectes();
+
+        a.AddArr(100000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
